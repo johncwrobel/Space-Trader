115e4eb207d8b6aa72baa77396db027483447b4c
GT-SEGFAULT <ditchfieldcaleb@gatech.edu>
2014-09-11 20:16:59 -0400

    Created Character.java
    I have no idea what I'm doing I haven't coded java in yeaaaars

---
+++ src/space/trader/Character.java
@@ -0,0 +1,8 @@
+public class Character(int a, int b, int c, int d, int e) {
+	private int Pilot = a;
+	private int Fighter = b;
+	private int Trader = c;
+	private int Engineer = d;
+	private int Investor = e;
+}
+ 
