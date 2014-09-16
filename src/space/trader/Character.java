7b6c92321281b293cd88bbc4f072a616c3549c90
tsolanki95 <tsolanki95@yahoo.in>
2014-09-16 15:23:29 -0400

    Change Character.java to follow understandable programming style

--- src/space/trader/Character.java
+++ src/space/trader/Character.java
@@ -1,8 +1,38 @@
-public class Character(int a, int b, int c, int d, int e) {
-	private int Pilot = a;
-	private int Fighter = b;
-	private int Trader = c;
-	private int Engineer = d;
-	private int Investor = e;
+package space.trader;
+
+public class Character {
+	private int pilot;
+	private int fighter;
+	private int trader;
+	private int engineer;
+	private int investor;
+        
+        public Character(int pilot, int fighter, int trader, int engineer, int investor) {
+            this.pilot = pilot;
+            this.fighter = fighter;
+            this.trader = trader;
+            this.engineer = engineer;
+            this.investor = investor;
+        }
+        
+        public int getPilot() {
+            return this.pilot;
+        }
+        
+        public int getFighter() {
+            return this.fighter;
+        }
+        
+        public int getTrader() {
+            return this.trader;
+        }
+        
+        public int getEngineer() {
+            return this.engineer;
+        }
+        
+        public int getInvestor() {
+            return this.investor;
+        }
 }
  
