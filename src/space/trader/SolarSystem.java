e5cbe79009d68d0f8d18d519fe36b90e7e60f647
Benjamin Cervantes <xervantes83@hotmail.com>
2014-09-22 12:05:00 -0400

    modified SolarSystem.java and added Planet.java

--- src/space/trader/SolarSystem.java
+++ src/space/trader/SolarSystem.java
@@ -1,4 +1,5 @@
 package space.trader;
+import java.util.*;
 
 /**
  *
@@ -9,11 +10,13 @@ public class SolarSystem {
     private String name;
     private int xLocation;
     private int yLocation;
+    private int numberOfPlanets;
 
-    public SolarSystem(String name, int xLocation, int yLocation) {
+    public SolarSystem(String name, int xLocation, int yLocation, int numberOfPlanets) {
         this.name = name;
         this.xLocation = xLocation;
         this.yLocation = yLocation;
+        this.numberOfPlanets = numberOfPlanets;
         
     }
     
@@ -43,6 +46,23 @@ public class SolarSystem {
 
     public void setyLocation(int yLocation) {
         this.yLocation = yLocation;
+    }
+    
+    public int getNumberOfPlanets() {
+        return this.numberOfPlanets;
+    }
+    
+    public void setNumberOfPlanets(int numberOfPlanets) {
+        this.numberOfPlanets = numberOfPlanets;
+    }
+    
+    public static void main(String args[]) {
+        ArrayList al = new ArrayList();
+        al.add("Solar System 1");
+        al.add("Solar System 2");
+        al.add("Solar System 3");
+        al.add("Solar System 4");
+        al.add("Solar System 5");
     }
 
 }
