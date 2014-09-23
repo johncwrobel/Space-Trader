a5fe11cef58a3e838289c8146e4e460426cedd7c
tsolanki95 <tsolanki95@yahoo.in>
2014-09-22 20:13:58 -0400

    fixed solar system class

--- src/space/trader/SolarSystem.java
+++ src/space/trader/SolarSystem.java
@@ -8,29 +8,14 @@ import java.util.*;
 public class SolarSystem {
 
     private String name;
-    private int xLocation;
-    private int yLocation;
-    private int numberOfPlanets;
-    private static ArrayList<String> systemNames = new ArrayList<String>();
-   
-    static {
-    systemNames.add("name1");
-    systemNames.add("name2");
-    systemNames.add("name3");
-    systemNames.add("name4");
-    systemNames.add("name5");
-    
-    }
+    private int x;
+    private int y;
     
     
-    public SolarSystem() {
-        Random r1 = new Random();
-        int random = r1.nextInt(systemNames.size());
-        name = systemNames.get(random);
-        systemNames.remove(random);
-        xLocation = (int)(Math.random() * 100) + 1;
-        yLocation = (int)(Math.random() * 100) + 1;
-        numberOfPlanets = (int)(Math.random() * 3) + 1;
+    public SolarSystem(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
     }
 
     public String getName() {
@@ -38,15 +23,11 @@ public class SolarSystem {
     }
     
     public int getXLocation() {
-        return this.xLocation;
+        return this.x;
     }
     
     public int getYLocation() {
-        return this.yLocation;
-    }
-    
-    public int getNumberOfPlanets() {
-        return this.numberOfPlanets;
+        return this.y;
     }
 
 }
