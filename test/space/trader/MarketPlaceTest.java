8362fa34c62b42771afc4c0069c8b3b303c8ae53
tsolanki95 <tsolanki95@yahoo.in>
2014-11-11 11:04:02 -0500

    fixed PMD errors in JUnit

--- test/space/trader/MarketPlaceTest.java
+++ test/space/trader/MarketPlaceTest.java
@@ -6,12 +6,7 @@
 
 package space.trader;
 
-import java.util.ArrayList;
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
