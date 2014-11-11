b4e5d2ddb09fb5b3592bf2f6f27fd31f597c9d13
Tommy Lee <tlee@gatech.edu>
2014-11-10 22:26:35 -0500

    JUnit for ship.canADD()

---
+++ test/space/trader/CanAddTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space.trader;
+
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Tommy Lee
+ */
+public class CanAddTest {
+    /**
+     * Tests when the cargo is empty
+     */
+    @Test
+    public final void testCanAdd() {
+        Ship ship = new Ship();
+        ship.setMaxCargo(10);
+        
+        assertEquals(ship.canAdd(), true);
+    }
+    /**
+     * Tests when the cargo has items in it but still with room for more
+     */
+    @Test
+    public final void testCanAdd5() {
+        ArrayList<Item> cargoHold = new ArrayList();
+        Ship ship = new Ship();
+        ship.setMaxCargo(10);
+        
+        for (int x = 0; x < 5; x++) {
+            cargoHold.add(new Item("Test" + x, 0, 0, 0, 0, 0, 0));
+        }
+        
+        ship.setCargoHold(cargoHold);
+        
+        assertEquals(ship.canAdd(), true);
+    }
+    
+    /**
+     * Tests when the cargo is full
+     */
+    @Test
+    public final void testCanAdd10() {
+        ArrayList<Item> cargoHold = new ArrayList();
+        Ship ship = new Ship();
+        ship.setMaxCargo(10);
+        
+        for (int x = 0; x < 10; x++) {
+            cargoHold.add(new Item("Test" + x, 0, 0, 0, 0, 0, 0));
+        }
+        
+        ship.setCargoHold(cargoHold);
+        
+        assertEquals(ship.canAdd(), false);
+    }
+    
+    
+}
